978b69ad4b5f7fddf998330778d8d40d28b97efd
Evgenii Molov <evgenii.molov@auto1.com>
2020-01-21 18:37:37 +0100

    Spring Initializr project init

---
+++ src/main/java/test/exampls/zenhomes/ZenhomesApplication.java
@@ -0,0 +1,13 @@
+package test.exampls.zenhomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenhomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenhomesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/exampls/zenhomes/ZenhomesApplicationTests.java
@@ -0,0 +1,13 @@
+package test.exampls.zenhomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenhomesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
